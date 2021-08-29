fc87d0bee9fc4d7d9d5282f47d1c3d7ee530a578
tmzpdpdlwl <tmzpdpdlwl@gmail.com>
2021-08-29 22:05:35 +0900

    style: Replace import function to module

--- src/Router.js
+++ src/Router.js
@@ -1,30 +1,35 @@
 import { createWebHistory, createRouter } from "vue-router";
+import Home from "./components/Home.vue";
+import About from "./components/About.vue";
+import Skill from "./components/Skill.vue";
+import Project from "./components/Project.vue";
+import Contact from "./components/Contact.vue";
 
 const routes = [
   {
     path: "/",
     name: "Home",
-    component: () => import("./components/Home.vue"),
+    component: Home,
   },
   {
     path: "/About",
     name: "About",
-    component: () => import("./components/About.vue"),
+    component: About,
   },
   {
     path: "/Skill",
     name: "Skill",
-    component: () => import("./components/Skill.vue"),
+    component: Skill,
   },
   {
     path: "/Project",
     name: "Project",
-    component: () => import("./components/Project.vue"),
+    component: Project,
   },
   {
     path: "/Contact",
     name: "Contact",
-    component: () => import("./components/Contact.vue"),
+    component: Contact,
   },
 ];
 
